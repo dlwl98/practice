e6cf5f35258b125e81cc9fbfe25532bded5281e2
dlwl98 <ff981113@hanyang.ac.kr>
2021-03-28 23:50:58 +0900

    한글!

--- algorithm/simple.cpp
+++ algorithm/simple.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <queue>
+
 using namespace std;
 
 int main(){
-    cout<<"df";
+    queue<int> card;
+    int n;
+    int tmp;
+    cin>>n;
+    for(int i=1;i<=n;i++) {card.push(i);}
+    while(card.size()-1){
+        card.pop();
+        tmp = card.front();
+        card.pop();
+        card.push(tmp);
+    }
+    cout<<card.front();
+    cout<<"\n한글";
 }
